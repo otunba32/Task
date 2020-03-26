317cb651c2a1760fe0bebd195a90ea483e3da9de
otunba32 <57312749+otunba32@users.noreply.github.com>
2020-03-26 07:57:37 +0100

    Add files via upload

---
+++ javascript task/js-task.js
@@ -0,0 +1,9 @@
+let name = "Emmmanuel";
+console.log("My name is: " + name);
+
+let courses = ["Python", "Html", "Css", "Javascript"];
+console.log("These are my courses: " + courses);
+
+for (let a = 1; a<=200; a++) {
+    if (a%2===0) console.log(a);
+}
