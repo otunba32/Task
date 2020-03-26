166ed32f1053c802905839975eaf61261631a6b3
otunba32 <57312749+otunba32@users.noreply.github.com>
2020-03-26 07:51:51 +0100

    Add files via upload

---
+++ python task/pythontask.py
@@ -0,0 +1,7 @@
+pi = 3.14
+radius = int(input('Enter the radius of a circle: '))
+area = pi * radius * radius
+circumference = 2 * pi * radius
+
+print("Area of a circle = %.2f" %area)
+print("Circumference of the Circle = %.2f" %circumference)
